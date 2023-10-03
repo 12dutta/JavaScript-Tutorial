a8937233bd971b46fb3f86ec3c2d0328c9908569
Vaibhav Dutta <88676932+12dutta@users.noreply.github.com>
2023-10-03 15:58:37 +0000

    comparisions of datatypes

---
+++ 01_basic/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// This were are simple comparision becoz they have numbers only at both positions
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// But in this types of comparision when string and numbers or string and boolean compare then there is difficult to solve
+
+// In most of the case this type of comparision were ignored becoz we want a readable code to present
+// **Clean code has more value then usual
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
